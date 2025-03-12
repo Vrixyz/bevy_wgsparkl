3ef1319c34ccc1d56a3abdb3d2df5054818b14d8
Thierry Berger <contact@thierryberger.com>
2025-03-12 16:36:02 +0100

    matrix configuration wip code

--- examples/cube.rs
+++ examples/cube.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 use bevy::render::renderer::RenderDevice;
+use bevy_editor_cam::DefaultEditorCamPlugins;
+use bevy_editor_cam::prelude::EditorCam;
 use bevy_rapier3d::geometry::RapierColliderHandle;
 use bevy_rapier3d::plugin::ReadRapierContext;
 use bevy_rapier3d::prelude::{Collider, RigidBody};
@@ -9,6 +11,7 @@ use bevy_wgsparkl::resources::{AppState, PhysicsContext};
 use nalgebra::{Vector3, vector};
 use wgrapier3d::dynamics::body::{BodyCoupling, BodyCouplingEntry};
 use wgsparkl3d::models::DruckerPrager;
+use wgsparkl3d::solver::ParticlePhase;
 use wgsparkl3d::{
     models::ElasticCoefficients,
     pipeline::MpmData,
@@ -17,7 +20,7 @@ use wgsparkl3d::{
 
 pub fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((DefaultPlugins, DefaultEditorCamPlugins))
         .add_plugins(bevy_rapier3d::plugin::RapierPhysicsPlugin::<()>::default())
         .add_plugins(RapierDebugRenderPlugin::default())
         .add_plugins(bevy_wgsparkl::WgSparklPlugin)
@@ -28,6 +31,10 @@ pub fn main() {
 pub fn setup_scene(mut commands: Commands) {
     commands.spawn((
         Camera3d::default(),
+        EditorCam {
+            last_anchor_depth: 110f64,
+            ..Default::default()
+        },
         Transform::from_xyz(-30.0, 30.0, 100.0).looking_at(Vec3::new(0.0, 10.0, 0.0), Vec3::Y),
     ));
     /*
@@ -68,9 +75,9 @@ pub fn setup_mpm_particles(
     let grid_size_x = 25;
     let grid_size_y = 25;
     let grid_size_z = 25;
-    let num_rocks = grid_size_x * grid_size_y * grid_size_z;
+    let num_particles = grid_size_x * grid_size_y * grid_size_z;
 
-    let rocks = (0..num_rocks)
+    let particle_positions = (0..num_particles)
         .into_iter()
         .map(|i| {
             let x = i % grid_size_x;
@@ -103,7 +110,7 @@ pub fn setup_mpm_particles(
     let device = device.wgpu_device();
 
     if !app_state.restarting {
-        app_state.num_substeps = 32;
+        app_state.num_substeps = 16;
         app_state.gravity_factor = 1.0;
     };
 
@@ -115,27 +122,46 @@ pub fn setup_mpm_particles(
     let cell_width = 1.0;
     let mut particles = vec![];
 
-    for rock in &rocks {
-        let position = vector![rock.x, rock.y, rock.z];
+    for x in -1..2 {
+        for z in -1..2 {
+            let x = x as f32;
+            let z = z as f32;
+            let offset = vector![x * grid_size_x as f32, 0f32, z * grid_size_z as f32] * 2f32;
+            for particle in &particle_positions {
+                let position = vector![particle.x, particle.y, particle.z];
 
-        let rock_size = vector![1.0, 1.0, 1.0];
-        let volume = rock_size.x * rock_size.y * rock_size.z;
-        let density = 700.0;
-        particles.push(Particle {
-            position: vector![position.x, position.y, position.z],
-            velocity: Vector3::zeros(),
-            volume: ParticleMassProps::new(density * volume, volume.cbrt() / 2.0),
-            model: ElasticCoefficients::from_young_modulus(10_000_000.0, 0.2),
-            plasticity: Some(DruckerPrager {
-                // TODO: tune these values.
-                h0: 45.0f32.to_radians(),
-                h1: 50.0f32.to_radians(),
-                h2: 0.4,
-                h3: 15.0f32.to_radians(),
-                ..DruckerPrager::new(10_000_000.0, 0.2)
-            }),
-            phase: None,
-        });
+                let particle_size = vector![1.0, 1.0, 1.0];
+                let volume = particle_size.x * particle_size.y * particle_size.z;
+                let density = 1700.0;
+                particles.push(Particle {
+                    position: nalgebra::Rotation::from_axis_angle(
+                        &Vector3::z_axis(),
+                        5f32.to_radians(),
+                    ) * vector![position.x, position.y, position.z]
+                        + offset,
+                    velocity: Vector3::zeros(),
+                    volume: ParticleMassProps::new(density * volume, volume.cbrt() / 2.0),
+                    model: ElasticCoefficients::from_young_modulus(
+                        100_000_000.0 * 10f32.powf((z - 1f32)),
+                        0.2 + x * 0.05f32,
+                    ),
+                    plasticity: Some(DruckerPrager {
+                        h0: (45.0f32 + x * 20f32).to_radians(),
+                        h1: (70.0f32 + x * 20f32).to_radians() + z,
+                        h2: 1.6 + z * 0.5f32,
+                        h3: 25.0f32.to_radians() + x,
+                        ..DruckerPrager::new(
+                            100_000_000.0 * 10f32.powf((z - 1f32)),
+                            0.2 + x * 0.05f32,
+                        )
+                    }),
+                    phase: Some(ParticlePhase {
+                        phase: 0.5 + 0.5 * x,
+                        max_stretch: (0.0 + z).clamp(0.0, 1.0),
+                    }),
+                });
+            }
+        }
     }
 
     println!("Number of simulated particles: {}", particles.len());
@@ -152,5 +178,8 @@ pub fn setup_mpm_particles(
         cell_width,
         60_000,
     );
-    commands.insert_resource(PhysicsContext { data, particles });
+    commands.insert_resource(PhysicsContext {
+        data,
+        particles: particles,
+    });
 }
